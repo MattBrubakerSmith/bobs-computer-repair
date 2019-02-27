cb687cc4e6c40999e2a1a6bf317d8819b72acb60
MattBrubakerSmith <matthewbrubakersmith@gmail.com>
2019-02-27 15:05:59 -0600

    made changes to FeaturedBobJob. Updated ServicesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule, MatGridListModule, MatDividerModule, MatListModule, MatCardModule } from "@angular/material";
+import { MatToolbarModule, MatMenuModule, MatButtonModule, MatIconModule, MatGridListModule, MatDividerModule, MatListModule, MatCardModule, MatCheckboxModule } from "@angular/material";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule }from "@angular/flex-layout";
 
@@ -13,7 +13,7 @@ import { ContactComponent } from './contact/contact.component';
 import { RouterModule } from '@angular/router';
 
 import { BobsServicesModule } from "./bobs-services/bobs-services.module";
-import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -38,7 +38,9 @@ import { HttpClientModule } from '@angular/common/http';
     MatCardModule,
     FlexLayoutModule,
     BobsServicesModule,
-    HttpClientModule
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/services/services.component.ts
+++ src/app/services/services.component.ts
@@ -1,26 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { BobJob } from '../bobs-services/bob-job';
 import { BobJobService } from '../bobs-services/bob-job.service';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
   styleUrls: ['./services.component.scss']
 })
-export class ServicesComponent implements OnInit {
+export class ServicesComponent {
   bobJobService: BobJobService;
   bobJobs: BobJob[];
-  selectedJobs: BobJob[] = []; 
+  repairServices: FormGroup;
 
   constructor(bobJobService: BobJobService) { 
     this.bobJobService = bobJobService;
     this.bobJobs = bobJobService.getAllBobJobs();
   }
 
-  ngOnInit() {
-  }
-
-  addJob(id: number) {
-    this.selectedJobs.push(this.bobJobService.getBobJobById(id));
+  onSubmit(formData) {
+    console.log(formData)
   }
 }
